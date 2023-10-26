bd8ea9740f3f7e06d4ece8472759bb5b8b6e0d6e
Rainn <56089814+AmbiakaTT@users.noreply.github.com>
2023-10-26 20:44:33 +0700

    Disable default security, test connecting with backend

--- src/main/java/com/vseven/launchpad/controller/DefaultController.java
+++ src/main/java/com/vseven/launchpad/controller/DefaultController.java
@@ -2,15 +2,18 @@ package com.vseven.launchpad.controller;
 
 
 import com.azure.core.annotation.Get;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+//@CrossOrigin("http://localhost:3000")
 public class DefaultController {
-
+    
     @GetMapping("/")
-    public String home() {
-        return "Welcome to VSeven Launchpad";
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello, World!");
     }
 }
