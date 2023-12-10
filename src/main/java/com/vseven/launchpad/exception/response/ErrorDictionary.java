1823844cf443371b301c0b20ca472691554751a4
khaukhau <20kiet.kl@vinuni.edu.vn>
2023-12-10 11:04:14 +0700

    [Tony] Change the error message to be more comprehensive

--- src/main/java/com/vseven/launchpad/exception/response/ErrorDictionary.java
+++ src/main/java/com/vseven/launchpad/exception/response/ErrorDictionary.java
@@ -3,9 +3,8 @@ package com.vseven.launchpad.exception.response;
 public enum ErrorDictionary {
     NF_001("QuickLink not found"),
     NF_002("User not found"),
-    NF_003("Link not found in QuickLinks"),
-    NF_004("User already bookmarked QuickLink"),
-
+    NF_003("Link not found in QuickLink"),
+    NF_004("Link already bookmarked in QuickLink"),
     NF_005("Link not found");
     private String errorMessage;
 
