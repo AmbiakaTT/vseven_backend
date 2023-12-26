715e7b00dbc8f0ac40fa52652bc1859bb7b74534
khaukhau <20kiet.kl@vinuni.edu.vn>
2023-12-26 13:55:59 +0700

    [Tony] Unit test for API Save

--- src/test/java/unit/com/vseven/launchpad/controller/QuickLinkControllerTest.java
+++ src/test/java/unit/com/vseven/launchpad/controller/QuickLinkControllerTest.java
@@ -1,12 +1,11 @@
 package unit.com.vseven.launchpad.controller;
 
 import com.vseven.launchpad.controller.QuickLinkController;
+import com.vseven.launchpad.dto.response.FullResponse;
+import com.vseven.launchpad.dto.response.MessageResponse;
 import com.vseven.launchpad.dto.response.SectionOrderResponse;
 import com.vseven.launchpad.entity.User;
-import com.vseven.launchpad.repository.LinkOrderRepository;
-import com.vseven.launchpad.repository.SectionOrderRepository;
-import com.vseven.launchpad.repository.UserQuickLinkRepository;
-import com.vseven.launchpad.repository.UserRepository;
+import com.vseven.launchpad.repository.*;
 import org.apache.coyote.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import unit.com.vseven.launchpad.utils.MockDataUtils;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -38,6 +38,10 @@ public class QuickLinkControllerTest {
     private SectionOrderRepository sectionOrderRepository;
     @Mock
     private LinkOrderRepository linkOrderRepository;
+    @Mock
+    private LinkRepository linkRepository;
+    @Mock
+    private SectionRepository sectionRepository;
 
 
     @BeforeEach
@@ -53,10 +57,33 @@ public class QuickLinkControllerTest {
         when(linkOrderRepository.findByUserUserName(any())).thenReturn(MockDataUtils.MOCK_LINK_ORDER_LIST);
 
         String userName = "test user";
-        ResponseEntity<?> response = controller.getQuickLinks(userName);
+        ResponseEntity<FullResponse> response = controller.getQuickLinks(userName);
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody().toString(), is(MockDataUtils.MOCK_FULL_RESPONSE.toString()));
     }
+
+    @Test
+    public void saveAPI_validRequest_returnSuccess() throws Exception{
+        when(userRepository.findByUserName(any())).thenReturn(MockDataUtils.MOCK_USER);
+        when(linkRepository.findByLinkId(any())).thenReturn(MockDataUtils.MOCK_LINK_OPTIONAL);
+        when(userQuickLinkRepository.findByUserNameAndLinkId(any(), any())).thenReturn(MockDataUtils.MOCK_USER_QUICK_LINK_OPTIONAL);
+        when(sectionRepository.findById(any())).thenReturn(MockDataUtils.MOCK_SECTION_OPTIONAL);
+        when(sectionRepository.count()).thenReturn(1L);
+        when(linkRepository.countLinksBySectionIds(any())).thenReturn(1);
+        //when(userQuickLinkRepository.save(MockDataUtils.MOCK_USER_QUICK_LINK)).thenReturn(MockDataUtils.MOCK_USER_QUICK_LINK);
+        //when(linkOrderRepository.findById(any())).thenReturn(Optional.of(MockDataUtils.MOCK_LINK_ORDER_ENTITY));
+        //when(sectionOrderRepository.findByUserUserName(any())).thenReturn(MockDataUtils.MOCK_SECTION_ORDER_LIST);
+        //when(linkOrderRepository.findByLinkId(any())).thenReturn(MockDataUtils.MOCK_LINK_ORDER_LIST);
+
+
+        String userName = "test user";
+        ResponseEntity<MessageResponse> response = controller.saveUserQuickLinks(userName, MockDataUtils.MOCK_COMBINED_DTO);
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody().toString(), is(MockDataUtils.MOCK_SAVE_MESSAGE_RESPONSE.toString()));
+
+    }
+
+
 
 }
--- src/test/java/unit/com/vseven/launchpad/utils/MockDataUtils.java
+++ src/test/java/unit/com/vseven/launchpad/utils/MockDataUtils.java
@@ -1,14 +1,16 @@
 package unit.com.vseven.launchpad.utils;
 
-import com.vseven.launchpad.dto.response.FullResponse;
-import com.vseven.launchpad.dto.response.LinkOrderResponse;
-import com.vseven.launchpad.dto.response.LinkResponse;
-import com.vseven.launchpad.dto.response.SectionOrderResponse;
+import com.vseven.launchpad.dto.request.CombinedDTO;
+import com.vseven.launchpad.dto.request.LinkOrderDTO;
+import com.vseven.launchpad.dto.request.QuickLinkDTO;
+import com.vseven.launchpad.dto.request.SectionOrderDTO;
+import com.vseven.launchpad.dto.response.*;
 import com.vseven.launchpad.entity.*;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class MockDataUtils {
     public static Integer MOCK_LINK_ID = 1;
@@ -19,36 +21,77 @@ public class MockDataUtils {
     public static Integer MOCK_LINK_ORDER = 1;
     public static Integer MOCK_SECTION_ORDER = 1;
     public static String MOCK_USER_NAME = "test user";
+    public static Integer MOCK_USER_ID = 1;
     public static User MOCK_USER = User.builder()
+            .userId(MOCK_USER_ID)
             .userName(MOCK_USER_NAME)
             .build();
+
+    // Link Entity
     public static Link MOCK_LINK = Link.builder()
             .linkId(MOCK_LINK_ID)
             .url(MOCK_URL)
             .linkName(MOCK_LINK_NAME)
             .sectionId(MOCK_SECTION_ID)
             .build();
+    public static Optional<Link> MOCK_LINK_OPTIONAL = Optional.of(MOCK_LINK);
+
+    // UserQuickLink entity
     public static UserQuickLink MOCK_USER_QUICK_LINK = UserQuickLink.builder()
             .link(MOCK_LINK)
+            .user(MOCK_USER)
             .build();
     public static List<UserQuickLink> MOCK_USER_QUICK_LINK_LIST = Arrays.asList(MOCK_USER_QUICK_LINK);
+    public static Optional<UserQuickLink> MOCK_USER_QUICK_LINK_OPTIONAL = Optional.of(MOCK_USER_QUICK_LINK);
+
+    // Section Entity
     public static Section MOCK_SECTION = Section.builder()
             .sectionId(MOCK_SECTION_ID)
             .sectionName(MOCK_SECTION_NAME)
             .build();
+    public static Optional<Section> MOCK_SECTION_OPTIONAL = Optional.of(MOCK_SECTION);
+
+    //SectionOrder entity
     public static SectionOrder MOCK_SECTION_ORDER_ENTITY = SectionOrder.builder()
             .section(MOCK_SECTION)
             .sectionOrder(MOCK_SECTION_ORDER)
             .user(MOCK_USER)
-
             .build();
     public static List<SectionOrder> MOCK_SECTION_ORDER_LIST = Arrays.asList(MOCK_SECTION_ORDER_ENTITY);
+
+    // LinkOrder entity
     public static LinkOrder MOCK_LINK_ORDER_ENTITY = LinkOrder.builder()
             .link(MOCK_LINK)
             .linkOrder(MOCK_LINK_ORDER)
             .section(MOCK_SECTION)
             .build();
     public static List<LinkOrder> MOCK_LINK_ORDER_LIST = Arrays.asList(MOCK_LINK_ORDER_ENTITY);
+
+    // DTO (Request body)
+    public static QuickLinkDTO MOCK_QUICK_LINK_DTO = QuickLinkDTO.builder()
+            .linksId(Arrays.asList(MOCK_LINK_ID))
+            .build();
+
+    public static LinkOrderDTO MOCK_LINK_ORDER_DTO = LinkOrderDTO.builder()
+            .sectionId(MOCK_SECTION_ID)
+            .userId(MOCK_USER_ID)
+            .linkId(MOCK_LINK_ID)
+            .linkOrder(MOCK_LINK_ORDER)
+            .build();
+
+    public static SectionOrderDTO MOCK_SECTION_ORDER_DTO = SectionOrderDTO.builder()
+            .sectionId(MOCK_SECTION_ID)
+            .userId(MOCK_USER_ID)
+            .order(MOCK_SECTION_ORDER)
+            .build();
+
+    public static CombinedDTO MOCK_COMBINED_DTO = CombinedDTO.builder()
+            .quickLinkDTO(MOCK_QUICK_LINK_DTO)
+            .linkOrderDTOList(Arrays.asList(MOCK_LINK_ORDER_DTO))
+            .sectionOrderDTOList(Arrays.asList(MOCK_SECTION_ORDER_DTO))
+            .build();
+
+    // Response
     public static LinkResponse MOCK_LINK_RESPONSE = LinkResponse.builder()
             .linkId(MOCK_LINK_ID)
             .linkName(MOCK_LINK_NAME)
@@ -76,6 +119,10 @@ public class MockDataUtils {
             .quickLink(MOCK_QUICK_LINK)
             .linkOrderResponse(MOCK_LINK_ORDER_RESPONSE_LIST)
             .sectionOrderResponse(MOCK_SECTION_ORDER_RESPONSE_LIST)
+            .build();
+
+    public static MessageResponse MOCK_SAVE_MESSAGE_RESPONSE = MessageResponse.builder()
+            .message("Successfully Saved")
             .build();
 
 
